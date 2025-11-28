95b4df5b84364ad899333b7a6090df69d1563b60
Rudra <yadavrudra00010@gmail.com>
2025-11-28 22:54:30 +0530

    Update Reciept Generator.py

--- Reciept Generator.py
+++ Reciept Generator.py
@@ -1,10 +1,26 @@
-sum=0
-while(True):
-    UserInput=input("enter the price: ")
-    if(UserInput!="q"):
-        sum = sum+ int(UserInput)
-        print(f"Order Total So Far {sum}")
-    else:
-        print("Thanks for Using our service kindly visit us again! /n your total bill is: ",sum)
-        break
+# sum=0
+# while(True):
+#     UserInput=input("enter the price: ")
+#     if(UserInput!="q"):
+#         sum = sum+ int(UserInput)
+#         print(f"Order Total So Far {sum}")
+#     else:
+#         print("Thanks for Using our service kindly visit us again! /n your total bill is: ",sum)
+#         break
     
+
+
+import sysconfig
+
+p=("pal pal jeena muhal mera tere bina /n "
+"ye saare nashe bekar teri aankho/n "
+"ke siva karne jaata mai bahar /n"
+"rehta tera intezaar mere /n"
+"khwabon me aa na kar ke /n "
+"solah sringar"
+
+
+
+)
+breaks=[0.083,0.083,0.087]
+print(p)
